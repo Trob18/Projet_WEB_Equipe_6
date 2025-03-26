f59721adba7a0b37ae1321dd62f5f6de7a0da94e
bastienldx <bastien.ledoux@viacesi.fr>
2025-03-26 16:28:25 +0100

    Add files via upload

---
+++ config/ConfigDatabase.php
@@ -0,0 +1,9 @@
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=projet_web', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
