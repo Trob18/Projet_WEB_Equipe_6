552fe1f59658d1adcc6204e4db2179083e78bd6b
bastienldx <bastien.ledoux@viacesi.fr>
2025-03-28 11:57:19 +0100

    Update AccountModel.php

--- app/Model/AccountModel.php
+++ app/Model/AccountModel.php
@@ -13,8 +13,24 @@ class Account {
         $this->pdo = $pdo;
     }
 
+    public function getAccount($column, $value, $selectColumn = '*') {
+        
+        $validColumns = ['Id_Account', 'LastName_Account', 'FirstName_Account', 'Email_Account', 'Password_Account', 'Image_Account', 'Description_Account', 'Address_Account', 'PhoneNumber_Account', 'Studies_Account', 'Id_Roles'];
+        if (!in_array($column, $validColumns)) {
+            return "Colonne invalide!";
+        }
+    
+        // Sélectionner la colonne spécifique demandée (par défaut '*')
+        $stmt = $this->pdo->prepare("SELECT $selectColumn FROM Accounts WHERE $column = :value LIMIT 1");
+        $stmt->execute(['value' => $value]);
+    
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    
+
     public function getAllAccounts() {
-        $stmt = $this->pdo->query("SELECT * FROM Accounts");
+        $stmt = $this->pdo->prepare("SELECT * FROM Accounts");
+        $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
@@ -29,14 +45,8 @@ class Account {
         return $stmt->execute([$lastName, $firstName, $email, $hashedPassword]);
     }
 
-    public function getAccountByEmail($email) {
-        $stmt = $this->pdo->prepare("SELECT * FROM Accounts WHERE Email_Account = :email LIMIT 1");
-        $stmt->execute(['email' => $email]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-
     public function removeAccount($id) {
-        $stmt = $this->pdo->prepare("DELETE FROM Accounts WHERE Id_Account = ?");
+        $stmt = $this->pdo->prepare("DELETE FROM Accounts WHERE Id_Account =    ?");
         return $stmt->execute([$id]);
     }
 
