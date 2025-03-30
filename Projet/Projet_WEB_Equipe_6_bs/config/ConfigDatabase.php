1b4af08ac2b7915adb1c032ae709e3fd31a82a36
Lenomade27 <baptisteselle@gmail.com>
2025-03-30 16:17:05 +0200

    Rename le nom de la database

--- Projet/Projet_WEB_Equipe_6_bs/config/ConfigDatabase.php
+++ Projet/Projet_WEB_Equipe_6_bs/config/ConfigDatabase.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=projet', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=livrableweb', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
 } catch (PDOException $e) {
