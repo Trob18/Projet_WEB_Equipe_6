572e053e3214899a9fd1af835d3a74851c137109
Lenomade27 <baptisteselle@gmail.com>
2025-03-31 10:30:40 +0200

    Supression des fichiers projet

--- Projet/Projet_WEB_Equipe_6_bs/config/ConfigDatabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=livrableweb', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-?>
